aa58125b92242c1750e2f1e2cb01576ee0e61fcb
tnemet <152886674+tnemet@users.noreply.github.com>
2024-01-10 10:05:41 +0100

    Add files via upload

---
+++ 00mail_IP_at_wakeup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+case $1/$2 in
+  pre/*)
+        next_day=$(date +%Y%m%d -d "$DATE + 1 day")
+        hour=40000
+        wake_time=$next_day"0"$hour
+        sudo rtcwake -m mem --date $wake_time
+    ;;
+  post/*)
+    bash path_to/mail_IP.sh
+    ;;
+esac
+
