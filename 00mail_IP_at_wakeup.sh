fe53a059fcce9ea4a177cfca8d48f84229c2b12a
tnemet <152886674+tnemet@users.noreply.github.com>
2024-01-10 10:07:27 +0100

    mail_IP

--- 00mail_IP_at_wakeup.sh
+++ 00mail_IP_at_wakeup.sh
@@ -2,7 +2,7 @@
 case $1/$2 in
   pre/*)
         next_day=$(date +%Y%m%d -d "$DATE + 1 day")
-        hour=40000
+        hour=50000
         wake_time=$next_day"0"$hour
         sudo rtcwake -m mem --date $wake_time
     ;;
