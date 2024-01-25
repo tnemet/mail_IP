35ec99a00ed5b81c8d848f2b351de74f4fc2e641
tnemet <152886674+tnemet@users.noreply.github.com>
2024-01-25 10:38:18 +0100

    adjusting
    removed unnecessary "go to sleep" option in pre) case

--- 00mail_IP_at_wakeup.sh
+++ 00mail_IP_at_wakeup.sh
@@ -2,12 +2,11 @@
 case $1/$2 in
   pre/*)
         next_day=$(date +%Y%m%d -d "$DATE + 1 day")
-        hour=50000
+        hour=80000
         wake_time=$next_day"0"$hour
-        sudo rtcwake -m mem --date $wake_time
+        sudo rtcwake -m no --date $wake_time
     ;;
   post/*)
-    source /path_to/mail_IP.sh
+    source /path to/check_IP.sh
     ;;
 esac
-
