5b5e143a9fcf3a0a1b8cab5a10589a6bcdc2fdcf
tnemet <152886674+tnemet@users.noreply.github.com>
2024-02-14 07:02:07 +0100

    simplified IP check

---
+++ check_IP.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+    current_IP=$( curl ifconfig.me | echo "$( awk -F. '{ print $1$2$3$4 }')" )
+    last_IP=$( cat /path to/last_IP.txt )
+    
+    if [ "$current_IP" -ne "$last_IP" ]; then 
+    
+    echo "Subject: IP_adress" > /path to/mail.txt
+    curl ifconfig.me >> /path to/mail.txt              
+    cat /path to/mail.txt | msmtp user@domain.com
+    
+    last_IP=$( curl ifconfig.me | echo "$( awk -F. '{ print $1$2$3$4 }')" )
+    echo "$last_IP" > /path to/last_IP.txt
+
+    fi
